6ce0d52b53b9ab403da8705903c19417c9c33346
victorisimoo <me@victorisimo.com>
2022-04-30 21:16:09 -0600

    I add the complete user registration method

--- src/com/netflix/controller/RegisterController.java
+++ src/com/netflix/controller/RegisterController.java
@@ -1,49 +1,85 @@
 package com.netflix.controller;
 
-import com.netflix.app.ParameterStringBuilder;
 import com.netflix.app.Principal;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable {
 
     private Principal principalScene;
-    @FXML private PasswordField txtPassword;
-    @FXML private TextField txtEmail;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private TextField txtEmail;
 
+    /**
+     * Initializes the controller class.
+     */
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) { }
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
 
-    public void setPrincipalScene(Principal principalScene){
+    /**
+     * Sets the scene
+     *
+     * @param principalScene
+     */
+    public void setPrincipalScene(Principal principalScene) {
         this.principalScene = principalScene;
     }
 
-    public Principal getPrincipalScene(){
-        return principalScene;
-    }
+    /**
+     * Register a new user
+     */
+    public void registerUser() {
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
 
-    public void loginUser(){ principalScene.homeScene();}
+        HttpURLConnection conn = null;
+        DataOutputStream os;
 
-    public void createUser(){
-        String user = txtEmail.getText();
-        String pass = txtPassword.getText();
-        if(api_insertUser(user, pass) == 200){
-            loginUser();
-        } // No se hizo correctamente el registro del usuario
-    }
+        try{
+            URL url = new URL("http://127.0.0.1:5000/create_user/");
+            String[] inputData = {"{\"name\": "+(char)34 + email + (char)34+", \"contrasena\": "+(char)34 + password + (char)34+"}"};
+            for(String input: inputData){
+                byte[] postData = input.getBytes(StandardCharsets.UTF_8);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty( "charset", "utf-8");
+                conn.setRequestProperty("Content-Length", Integer.toString(input.length()));
+                os = new DataOutputStream(conn.getOutputStream());
+                os.write(postData);
+                os.flush();
 
-    public int api_insertUser(String username, String password) {
-        return 200;
+                if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+                }
+
+                BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                String output;
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                conn.disconnect();
+                principalScene.homeScene();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally  {
+            if(conn != null)  {
+                conn.disconnect();
+            }
+        }
     }
 }
